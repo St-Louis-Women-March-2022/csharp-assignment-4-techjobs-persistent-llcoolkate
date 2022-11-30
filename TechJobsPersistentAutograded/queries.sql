557f227d788f35f98484c26a354ad27bd2d3642d
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-11-30 16:26:09 -0600

    Tim Ream

--- TechJobsPersistentAutograded/queries.sql
+++ TechJobsPersistentAutograded/queries.sql
@@ -1,13 +1,13 @@
-﻿--Part 1
-SELECT COLUMN_NAME, DATA_TYPE  FROM INFORMATION_SCHEMA.COLUMNS 
-  WHERE table_name = 'jobs'
-  --Employer ID, Id, Name
+﻿----Part 1
+--SELECT COLUMN_NAME, DATA_TYPE  FROM INFORMATION_SCHEMA.COLUMNS 
+--  WHERE table_name = 'jobs'
+--  --Employer ID, Id, Name
 
---Part 2
-SELECT employers.Name AS Employer, Location FROM employers
-WHERE Location = "Saint Louis";
---Part 3
-SELECT skills.Name, skills.Description FROM skills
-INNER JOIN jobskills ON skills.Id = jobskills.SkillId
-GROUP BY skills.Name
-ORDER BY skills.Name ASC;
+----Part 2
+--SELECT employers.Name AS Employer, Location FROM employers
+--WHERE Location = "Saint Louis";
+----Part 3
+--SELECT skills.Name, skills.Description FROM skills
+--INNER JOIN jobskills ON skills.Id = jobskills.SkillId
+--GROUP BY skills.Name
+--ORDER BY skills.Name ASC;
