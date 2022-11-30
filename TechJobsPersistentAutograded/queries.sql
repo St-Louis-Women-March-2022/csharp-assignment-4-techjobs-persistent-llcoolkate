ebe66fad72cd98f1ac72ef948d7af465af90ffb8
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-11-30 16:19:32 -0600

    sql

--- TechJobsPersistentAutograded/queries.sql
+++ TechJobsPersistentAutograded/queries.sql
@@ -4,5 +4,10 @@ SELECT COLUMN_NAME, DATA_TYPE  FROM INFORMATION_SCHEMA.COLUMNS
   --Employer ID, Id, Name
 
 --Part 2
-
+SELECT employers.Name AS Employer, Location FROM employers
+WHERE Location = "Saint Louis";
 --Part 3
+SELECT skills.Name, skills.Description FROM skills
+INNER JOIN jobskills ON skills.Id = jobskills.SkillId
+GROUP BY skills.Name
+ORDER BY skills.Name ASC;
